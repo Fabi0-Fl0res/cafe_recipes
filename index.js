b3a50fea82637e395b2fe7ff982381664f088c87
Fabio Flores <fabioflores@ravn.co>
2023-04-04 09:21:25 -0600

    fix: rename README and remove server.js

--- index.js
+++
@@ -1,8 +0,0 @@
-//archivo para practicar
-function main() {
-  return 2 * 3;
-}
-
-const suma = (a, b) => {
-  return a + b;
-};
--- server.js
+++
@@ -1,7 +0,0 @@
-const express = required('express');
-
-const app = express();
-
-app.listen(3000, () => console.log('running port 3000'));
-
-modules.exports = app;
